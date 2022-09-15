99a75e94644aceb6f9b6b609530913054c737a78
KajalShendage <shendagekajal123@gmail.com>
2022-09-15 20:26:07 +0530

    [Pramod] Add: Create Simple Linked List

--- LinkedListProblem/Program.cs
+++ LinkedListProblem/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Datastructure using LinkedList, Stack & Queue!");
+﻿using LinkedListProblem;
+
+LinkedList list = new LinkedList();
+bool flag = true;
+while (flag)
+{
+    Console.WriteLine("Select Program\n 1.Create LinkedList");
+    int option = Convert.ToInt32(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.Display();
+            break;
+    }
+}
